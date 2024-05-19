46df560dc507ede5dcc2811d5ef2d2e49751ad85
Sam Weiler <denelfie@gmail.com>
2024-05-19 03:25:07 +0200

    after aperoli day commit

--- src/main.cpp
+++ src/main.cpp
@@ -1,260 +1,151 @@
 #include <Wire.h>
 #include <Joystick.h>
-#include <MPU6050.h>
+#include <MPU6050_light.h>
 
-MPU6050 mpu;
-int16_t accelerationX, accelerationY, accelerationZ;
-int16_t gyroX, gyroY, gyroZ;
+MPU6050 mpu(Wire);
+Joystick_ Joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
+                   1, 0,                   // Button Count, Hat Switch Count
+                   true, true, true,       // X and Y, and Z Axis
+                   true, true, true,       // Rx, Ry, and Rz
+                   false, false,           // rudder and throttle
+                   false, false, false);   // accelerator, brake, and steering
 
-int16_t accelerationXOffset, accelerationYOffset, accelerationZOffset;
-int16_t gyroXOffset, gyroYOffset, gyroZOffset;
 
-Joystick_ joystick(JOYSTICK_DEFAULT_REPORT_ID, JOYSTICK_TYPE_GAMEPAD,
-                   7, 0,                // 7 Buttons, no Hat Switch
-                   true, true, true,    // X, Y, and Z Axes
-                   true, true, true);   // Rx, Ry, and Rz for Rotation
-
-const int numReadings = 1500;
-const int gyroThreshold = 1000; // Adjusted from 5000 based on new gyro offset and sensitivity
-const int jumpThreshold = 2000; // Adjusted from 10000, consider the new sensitivity
-const int accelerationThreshold = 1000; // Adjusted from 5000
-const float leanThreshold = 100.0; // Degrees, reduced from 45 for sensitivity
-
-// mapping range variables
-int16_t rangeGyro = 7000;
-int16_t rangeAccelXY = 1000;
-int16_t rangeAccelZ = 4000;
-
-bool isPlayerPresent = false;
-
-float accelerationXDeadzone, accelerationYDeadzone, accelerationZDeadzone;
-float forwardCosine = 1.0;  // Default-value (no rotation)
-float forwardSine = 0.0;    // Default-value (no rotation)
-float alpha = 0.98;         // Complementary filter coefficient
-
-// Sensor fusion variables
-float pitch = 0, roll = 0;
-
-void calibrateSensors();
-void checkForMovement();
-void checkForSignificantLean();
-void applyOffsets();
-void applySensorFusion();
-void detectPlayer();
-void defineAxis();
+  
 void playInitSound();
 void playSuccessSound();
 void playSetupCompleteSound();
 
 void setup() {
-    Serial.begin(115200);
-    Wire.begin();
-    mpu.initialize();
-
-    if (!mpu.testConnection()) {
-        Serial.println("MPU6050 connection failed");
-        while (1); // Halt if connection failed
-    }
-     // Configure MPU6050 settings for accelerometer and gyroscope
-    mpu.setFullScaleAccelRange(MPU6050_ACCEL_FS_16); // Set accelerometer to +/- 16g
-    mpu.setFullScaleGyroRange(MPU6050_GYRO_FS_2000); // Set gyroscope to +/- 2000 deg/s
-    pinMode(8, OUTPUT);     // Set digital pin 8 as an OUTPUT for buzzer
-    calibrateSensors();
-    Serial.println("Device is calibrated.");
-
-    //detectPlayer();             // Ensure player is detected before proceeding
-    //checkForSignificantLean();  // Wait for player to lean forward
-    //defineAxis();               // Then define the axis based on the player's orientation
-
-    playSetupCompleteSound();
-    Serial.println("Setup complete. Device is ready to use!");
-    
-    joystick.begin();
+  Serial.begin(115200);
+  Wire.begin();
+  mpu.begin();    
+  pinMode(8, OUTPUT);     // Set digital pin 8 as an OUTPUT for buzzer
+  mpu.calcOffsets(true, true);
+  Joystick.begin(false);
+  playSetupCompleteSound();
 }
+
+  int16_t joystickXMax=-5000;
+  int16_t joystickYMax=-5000;
+  int16_t joystickZMax=-5000;
+  int16_t joystickRxMax=-5000;
+  int16_t joystickRyMax=-5000;
+  int16_t joystickRzMax=-5000;
+
+  int16_t joystickXMin=5102;
+  int16_t joystickYMin=5102;
+  int16_t joystickZMin=5102;
+  int16_t joystickRxMin=5012;
+  int16_t joystickRyMin=5012;
+  int16_t joystickRzMin=5012;
+
+  int16_t jumpThreshold=1020;
 
 void loop() {
-    static unsigned long lastMillis = 0;
-    if (millis() - lastMillis >= 10) {
-        lastMillis = millis();
-        mpu.getMotion6(&accelerationX, &accelerationY, &accelerationZ, &gyroX, &gyroY, &gyroZ);
-        
-        accelerationX = map(accelerationX, -rangeAccelXY, rangeAccelXY, 0, 1023);
-        accelerationY = map(accelerationY, -rangeAccelXY, rangeAccelXY, 0, 1023);
-        accelerationZ = map(accelerationZ, -rangeAccelZ, rangeAccelZ, 0, 1023);
-        gyroX = map(gyroX, -rangeGyro, rangeGyro, 0, 1023);
-        gyroY = map(gyroY, -rangeGyro, rangeGyro, 0, 1023);
-        gyroZ = map(gyroZ, -rangeGyro, rangeGyro, 0, 1023);
+  mpu.update();
+  float angleX = mpu.getAngleX(); // Roll
+  float angleY = mpu.getAngleY(); // Pitch
+  float accelZ = mpu.getAccZ();   // Use Z-acceleration to simulate vertical movement
+  float gyroX = mpu.getGyroX();   // Rotation rate (gyroscope X-axis)
+  float gyroY = mpu.getGyroY();   // Tilt rate (gyroscope Y-axis)
+  float gyroZ = mpu.getGyroZ();   // Yaw rate (gyroscope Z-axis)
 
-        //applySensorFusion();
-        checkForMovement();
-    }
-}
+  float accelZ2= accelZ *1000;
 
-void applyOffsets() {
-    accelerationX -= accelerationXOffset;
-    accelerationY -= accelerationYOffset;
-    accelerationZ -= accelerationZOffset;
-    gyroX -= gyroXOffset;
-    gyroY -= gyroYOffset;
-    gyroZ -= gyroZOffset;
-}
 
-void checkForMovement() {
+  Serial.print(F("angleX: ")); Serial.print(angleX);
+  Serial.print(F(", angleY: ")); Serial.print(angleY);
+  Serial.print(F(", accelZ: ")); Serial.print(accelZ);
+  Serial.print(F(", accelZ2: ")); Serial.print(accelZ2);
+  Serial.print(F(", gyroZ: ")); Serial.print(gyroX);
+  Serial.print(F(", gyroY: ")); Serial.print(gyroY);
+  Serial.print(F(", gyroZ: ")); Serial.print(gyroZ);
 
-    joystick.setXAxis(accelerationX);
-    joystick.setYAxis(accelerationY);
-    joystick.setZAxis(accelerationZ);
-    joystick.setRxAxis(gyroX);
-    joystick.setRyAxis(gyroY);
-    joystick.setRzAxis(gyroZ);
-}
+  // Normalize angles from [-90, 90] to [-1, 1]
+  float normalizedX = angleX / 30.0;
+  float normalizedY = angleY / 30.0;
+  float normalizedZ = (accelZ + 2) / 4.0;  // Normalize acceleration assuming range [-2, 2] g
 
-void calibrateSensors() {
-    delay(1000);
-    playInitSound();
+  int range = 5000;
 
-    Serial.println("Calibrating...");
-    int32_t accelerationXSum = 0, accelerationYSum = 0, accelerationZSum = 0;
-    int32_t gyroXSum = 0, gyroYSum = 0, gyroZSum = 0;
-    int16_t accelerationXReading, accelerationYReading, accelerationZReading;
-    int16_t gyroXReading, gyroYReading, gyroZReading;
+  // Apply a quadratic transformation (moderate exponential effect)
+  int16_t joystickX = (int)(511.5 * (pow(normalizedX, 2) * (normalizedX < 0 ? -1 : 1) + 1));
+  int16_t joystickY = (int)(511.5 * (pow(normalizedY, 2) * (normalizedY < 0 ? -1 : 1) + 1));
+  int16_t joystickZ;
+  int16_t joystickRx;
+  int16_t joystickRy;
+  int16_t joystickRz;
 
-    Serial.println("Ensure the device is stationary and flat on a level surface...");
 
-    for (int i = 0; i < numReadings; i++) {
-        mpu.getMotion6(&accelerationXReading, &accelerationYReading, &accelerationZReading,
-                       &gyroXReading, &gyroYReading, &gyroZReading);
+  // Ensure values are within bounds
+  joystickX = constrain(joystickX, 0, 1023);
+  joystickY = constrain(joystickY, 0, 1023);
+  joystickZ = map((int)(accelZ * 100), 0, 200, 0, 1023);
+  joystickRx = map(gyroX, -500, 500, 0, 1023);
+  joystickRy = map(gyroY, -500, 500, 0, 1023);
+  joystickRz = map(gyroZ, -500, 500, 0, 1023);
 
-        accelerationX = map(accelerationX, -1000, 1000, 0, 1023);
-        accelerationY = map(accelerationY, -1000, 1000, 0, 1023);
-        accelerationZ = map(accelerationZ, -8000, 8000, 0, 1023);
-        gyroX = map(gyroX, -7000, 7000, 0, 1023);
-        gyroY = map(gyroY, -7000, 7000, 0, 1023);
-        gyroZ = map(gyroZ, -7000, 7000, 0, 1023);
-        accelerationXSum += accelerationXReading;
-        accelerationYSum += accelerationYReading;
-        accelerationZSum += accelerationZReading;
-        gyroXSum += gyroXReading;
-        gyroYSum += gyroYReading;
-        gyroZSum += gyroZReading;
-        delay(5);
-    }
+  
+  joystickXMax = joystickX > joystickXMax ? joystickX : joystickXMax;
+  joystickYMax = joystickY > joystickYMax ? joystickY : joystickYMax;
+  joystickZMax = joystickZ > joystickZMax ? joystickZ : joystickZMax;
+  joystickRxMax = joystickRx > joystickRxMax ? joystickRx : joystickRxMax;
+  joystickRyMax = joystickRy > joystickRyMax ? joystickRy : joystickRyMax;
+  joystickRzMax = joystickRz > joystickRzMax ? joystickRz : joystickRzMax;
+  
+  joystickXMin = joystickX < joystickXMin ? joystickX : joystickXMin;
+  joystickYMin = joystickY < joystickYMin ? joystickY : joystickYMin;
+  joystickZMin = joystickZ < joystickZMin ? joystickZ : joystickZMin;
+  joystickRxMin = joystickRx < joystickRxMin ? joystickRx : joystickRxMin;
+  joystickRyMin = joystickRy < joystickRyMin ? joystickRy : joystickRyMin;
+  joystickRzMin = joystickRz < joystickRzMin ? joystickRz : joystickRzMin;
 
-    accelerationXOffset = -accelerationXSum / numReadings;
-    accelerationYOffset = -accelerationYSum / numReadings;
-    accelerationZOffset = -accelerationZSum / numReadings;
-    gyroXOffset = -gyroXSum / numReadings;
-    gyroYOffset = -gyroYSum / numReadings;
-    gyroZOffset = -gyroZSum / numReadings;
+   Serial.print(F(", joystickX: ")); Serial.print(joystickX);
+  Serial.print(F(", joystickY: ")); Serial.print(joystickY);
+  Serial.print(F(", joystickZ: ")); Serial.print(joystickZ);
+  Serial.print(F(", joystickRx: ")); Serial.print(joystickRx);
+  Serial.print(F(", joystickRy: ")); Serial.print(joystickRy);
+  Serial.print(F(", joystickRz: ")); Serial.println(joystickRz);
 
-    Serial.println("Calibration complete.");
-    Serial.println("Offsets:");
-    Serial.print("    AX: "); Serial.println(accelerationXOffset);
-    Serial.print("    AY: "); Serial.println(accelerationYOffset);
-    Serial.print("    AZ: "); Serial.println(accelerationZOffset);
-    Serial.print("    GX: "); Serial.println(gyroXOffset);
-    Serial.print("    GY: "); Serial.println(gyroYOffset);
-    Serial.print("    GZ: "); Serial.println(gyroZOffset);
 
-    playSuccessSound();
-}
+/*
 
-void checkForSignificantLean() {
-    Serial.println("Waiting for a significant lean to define the forward direction...");
-    playInitSound();
-    bool significantLeanDetected = false;
+Serial.print(F(", joystickX: ")); Serial.print(joystickXMax);
+  Serial.print(F(", joystickY: ")); Serial.print(joystickYMax);
+  Serial.print(F(", joystickZ: ")); Serial.print(joystickZMax);
+  Serial.print(F(", joystickRx: ")); Serial.print(joystickRxMax);
+  Serial.print(F(", joystickRy: ")); Serial.print(joystickRyMax);
+  Serial.print(F(", joystickRz: ")); Serial.print(joystickRzMax);
+  Serial.print("     ");
+Serial.print(F(", joystickX: ")); Serial.print(joystickXMin);
+  Serial.print(F(", joystickY: ")); Serial.print(joystickYMin);
+  Serial.print(F(", joystickZ: ")); Serial.print(joystickZMin);
+  Serial.print(F(", joystickRx: ")); Serial.print(joystickRxMin);
+  Serial.print(F(", joystickRy: ")); Serial.print(joystickRyMin);
+  Serial.print(F(", joystickRz: ")); Serial.println(joystickRzMin);
+*/
+  Joystick.setXAxis(joystickX);
+  Joystick.setYAxis(joystickY);
+  Joystick.setZAxis(joystickZ);
+  Joystick.setRxAxis(joystickRx);
+  Joystick.setRyAxis(joystickRy);
+  Joystick.setRzAxis(joystickRz);
 
-    while (!significantLeanDetected) {
-        mpu.getMotion6(&accelerationX, &accelerationY, &accelerationZ, &gyroX, &gyroY, &gyroZ);
-        applyOffsets();
-        applySensorFusion(); // Ensure sensor fusion is applied to update pitch and roll
+  if (joystickZ > jumpThreshold) {
+        Joystick.setButton(0, 1);
+        Serial.println("Jump detected with height: " + String(joystickZ));
+    } else Joystick.setButton(0, 0);
 
-        if (fabs(pitch) > leanThreshold || fabs(roll) > leanThreshold) {
-            Serial.println("Significant lean detected. Proceeding to define the axis...");
-            significantLeanDetected = true;
-            playSuccessSound();
-            break;
-        }
-        delay(10);
-    }
-}
-
-void defineAxis() {
-    Serial.println("Defining the forward axis based on the average lean direction.");
-    Serial.println("Please hold the position for a few seconds...");
-    unsigned long startTime = millis();
-    const int defineDuration = 5000;
-    float sumCosX = 0, sumSinY = 0;
-    int count = 0;
-
-    while (millis() - startTime < defineDuration) {
-        mpu.getMotion6(&accelerationX, &accelerationY, &accelerationZ, &gyroX, &gyroY, &gyroZ);
-        applyOffsets();
-        applySensorFusion(); // Ensure pitch and roll are updated
-
-        // Debugging outputs
-        Serial.print("Pitch: "); Serial.println(pitch);
-        Serial.print("Roll: "); Serial.println(roll);
-
-        // Trigonometric calculations with checks
-        if (roll * PI / 180 < PI && roll * PI / 180 > -PI) {
-            sumCosX += cos(roll * PI / 180) * cos(pitch * PI / 180); // Calculate forward direction based on stabilized pitch and roll
-            sumSinY += sin(roll * PI / 180);
-            count++;
-        } else {
-            Serial.println("Roll value out of bounds for trigonometric functions.");
-        }
-
-        delay(10);
-    }
-
-    if (count > 0) {
-        forwardCosine = sumCosX / count;
-        forwardSine = sumSinY / count;
-    } else {
-        forwardCosine = 0; // Default or error handling
-        forwardSine = 0;
-    }
-
-    Serial.print("Forward direction defined with cosine: ");
-    Serial.println(forwardCosine);
-    Serial.print("and sine: ");
-    Serial.println(forwardSine);
-    playSuccessSound();
-}
-
-void detectPlayer() {
-    delay(1000);
-    playInitSound();
-    int stableReadings = 0;
-    const int requiredStableReadings = 10;
-    float accelerationZThreshold = 5000; // Lowered threshold based on sensitivity
-
-    Serial.println("Waiting for player to sit down...");
-
-    while (!isPlayerPresent) {
-        mpu.getMotion6(&accelerationX, &accelerationY, &accelerationZ, &gyroX, &gyroY, &gyroZ);
-        applyOffsets();
-
-        if (abs(accelerationZ - 2048) > accelerationZThreshold) { // Adjusted for 16g sensitivity, assuming middle value around 2048
-            stableReadings++;
-            if (stableReadings >= requiredStableReadings) {
-                isPlayerPresent = true;
-                Serial.println("Player detected on chair.");
-                break;
-            }
-        } else {
-            stableReadings = 0;
-        }
-        delay(10);
-    }
-    playSuccessSound();
+  Joystick.sendState(); // Send the updated state to the host
+  delay(3);
 }
 
 void playInitSound() {
-    tone(8, 1000);
-    delay(150);
-    noTone(8);
+    // Buzzer
+    tone(8, 1000);  // Play tone at 1000 Hz
+    delay(150);     // Continue for 1 second
+    noTone(8);      // Stop the tone
     delay(150);
     tone(8, 1000);
     delay(150);
@@ -262,9 +153,9 @@ void playInitSound() {
 }
 
 void playSuccessSound() {
-    tone(8, 1000);
-    delay(150);
-    tone(8, 1200);
+    tone(8, 1000);  // Play tone at 1000 Hz
+    delay(150);     // Continue for 0.75 seconds
+    tone(8, 1200);  // Play tone at 1000 Hz
     delay(150);
     tone(8, 1400);
     delay(150);
@@ -272,15 +163,20 @@ void playSuccessSound() {
 }
 
 void playSetupCompleteSound() {
-    tone(8, 1000);
-    delay(300);
-    noTone(8);
-    delay(300);
-    tone(8, 1000);
-    delay(300);
-    noTone(8);
-    delay(300);
-    tone(8, 1200);
-    delay(300);
-    noTone(8);
+    // Play a simple ascending tone sequence to indicate completion
+    tone(8, 800);   // Play tone at 800 Hz
+    delay(150);     // Continue for 0.2 seconds
+    tone(8, 1000);  // Increase tone to 1000 Hz
+    delay(150);     // Continue for 0.2 seconds
+    tone(8, 1200);  // Increase tone further to 1200 Hz
+    delay(150);     // Continue for 0.2 seconds
+    noTone(8);      // Stop the tone
+
+    // Add a short pause
+    delay(150);
+
+    // Play a final high tone to definitively signal readiness
+    tone(8, 1500);  // Play a higher tone at 1500 Hz
+    delay(300);     // Continue for 0.3 seconds
+    noTone(8);      // Stop the tone
 }
